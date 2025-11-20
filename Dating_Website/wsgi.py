b8cc166bd0a52d1747d92d13b931bcad8873d602
nadeemmaahmud <nadeemmaahmud@gmail.com>
2025-11-21 05:42:53 +0600

    Fix ASGI/WSGI conflict - ensure WSGI is used

--- Dating_Website/wsgi.py
+++ Dating_Website/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 
+# Add the project directory to the Python path
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dating_Website.settings')
 
+# Get the WSGI application
 application = get_wsgi_application()
