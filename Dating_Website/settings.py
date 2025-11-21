f7463252adb368634f3497e4de1ff717faa554fd
nadeemmaahmud <nadeemmaahmud@gmail.com>
2025-11-21 06:10:02 +0600

    Add logging configuration to diagnose errors

--- Dating_Website/settings.py
+++ Dating_Website/settings.py
@@ -35,6 +35,28 @@ CSRF_TRUSTED_ORIGINS = [
     'https://loveconnect-q6uk.onrender.com',
 ]
 
+# Logging configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
 
 # Application definition
 
