8a88cac6a603d6b9ad17d0cc23094c8837273ef9
nadeemmaahmud <nadeemmaahmud@gmail.com>
2025-11-21 06:21:54 +0600

    Add comments to build script - trigger redeploy with PostgreSQL

--- build.sh
+++ build.sh
@@ -2,10 +2,14 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
 # Create staticfiles directory if it doesn't exist
 mkdir -p staticfiles
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run database migrations
 python manage.py migrate
