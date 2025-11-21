41689e4d236187444dc8c5c38f1108c4e25cd6ca
nadeemmaahmud <nadeemmaahmud@gmail.com>
2025-11-21 07:37:44 +0600

    Fix INSTALLED_APPS order - cloudinary_storage before staticfiles

--- Dating_Website/settings.py
+++ Dating_Website/settings.py
@@ -66,8 +66,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',  # Must be before staticfiles
     'django.contrib.staticfiles',
-    'cloudinary_storage',
     'cloudinary',
     'main',
     'user',
