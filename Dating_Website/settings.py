2840ceafc636701b4f79f2b9550f8a68b978992f
nadeemmaahmud <nadeemmaahmud@gmail.com>
2025-11-21 06:05:23 +0600

    Add CSRF trusted origins for Render

--- Dating_Website/settings.py
+++ Dating_Website/settings.py
@@ -29,6 +29,12 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+# CSRF Trusted Origins for Render
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.onrender.com',
+    'https://loveconnect-q6uk.onrender.com',
+]
+
 
 # Application definition
 
